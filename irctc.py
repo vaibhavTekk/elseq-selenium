7b6f1d438c5e6c446a3385ed0667d597dac447bd
Vaibhav Tekkalur S <tekkalurvaibhav@gmail.com>
2024-12-18 13:15:38 +0530

    scraping done

--- irctc.py
+++ irctc.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
@@ -21,6 +21,8 @@ dest.send_keys("DELHI - DLI (NEW DELHI)")
 dest.send_keys(Keys.TAB)
 
 date_field = driver.find_element(By.XPATH, '//*[@id="jDate"]/span/input')
+date_field.clear()
+date_field.send_keys(Keys.CONTROL + 'a')
 date_field.send_keys(Keys.BACKSPACE)
 date_field.send_keys("20/12/2024")
 date_field.send_keys(Keys.TAB)
@@ -33,9 +35,19 @@ option.click()
 
 form = driver.find_element(By.XPATH,'//*[@id="divMain"]/div/app-main-page/div/div/div[1]/div[1]/div[1]/app-jp-input/div/form')
 form.submit()
+# //*[@id="divMain"]/div/app-train-list//app-train-avl-enq
 
-elements = driver.find_elements("xpath", "//*[contains(@class, 'train-heading')]")
-print(elements)
+elem = WebDriverWait(driver, 30).until(
+EC.presence_of_element_located((By.XPATH, "//app-train-list//app-train-avl-enq")) #This is a dummy element
+)
+
+elements = driver.find_elements(By.XPATH, "//app-train-list//app-train-avl-enq")
+# print(elements)
 # Loop through and interact with each element
 for element in elements:
-    print(element.text)
+    el = element.find_elements(By.TAG_NAME, "strong")
+    [print(e.text) for e in el]
+
+
+# QFZrM9t7QUzigWD
+# 9174287232
